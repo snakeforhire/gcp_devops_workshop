f64f2200e0d275632140a36d41e22600aafa33f7
snakeforhire <regis.herrmann@gmail.com>
2023-04-24 11:37:05 +0200

    chg tst

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,7 +4,7 @@ import request from "supertest";
 describe("GET /", () => {
   it("should return 200 OK", async () => {
     const response = await request(app).get("/");
-    expect(response.status).toEqual(200);
+    expect(response.status).toEqual(500);
     expect(response.body.data).toBeNull();
     expect(response.body.msg).toEqual("API is up");
   });
