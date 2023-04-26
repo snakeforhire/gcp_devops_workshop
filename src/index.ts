be5b780c47f70a7baba6f9ab92ddc105e4ea987a
snakeforhire <regis.herrmann@gmail.com>
2023-04-26 14:37:32 +0200

    add errcode

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,17 @@ API.get("/", (req: Request, res: Response) => {
   });
 });
 
+API.get("/error", (req: Request, res: Response) => {
+  res.status(500).json({
+    msg: "API is down",
+    data: null,
+  });
+});
+
+API.get("/crash", (req: Request, res: Response) => {
+  throw new Error("api has crashed");
+});
+
 export const server = API.listen(8080, () => {
   console.log("server started on port 8080");
 });
